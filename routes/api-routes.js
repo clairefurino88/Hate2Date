75cf3b035b786f8ddf52694ed11d06b207328f54
clairefurino88 <33135335+clairefurino88@users.noreply.github.com>
2018-04-28 21:24:40 -0400

    added api routing for post

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,32 +1,24 @@
 // Requiring Models (Sequelize) and Passport (Login Authentication)
 var db = require("../models");
 var passport = require("../config/passport.js");
-
 // Requiring Sequelize for query ordering in 'Post' model 'get' route
 var Sequelize = require("sequelize");
-
 // App Routes
 module.exports = function (app) {
-
     // Passport Login Authentication Route
     // If valid login, go to members page, otherwise, display error.
     app.post("/api/login", passport.authenticate("local"), function (req, res) {
-
         // Since we're doing a POST with JS, we can't actually redirect POST to GET
         // so we're sending user back to members page with front-end redirect
         // User will not hit this page if not authenticated
         res.json("/members");
-
     });
-
     // User Sign-Up Route
     // User password automatically encrypted\hashed with 'BCrypt."
     // Upon successful user creation, proceed to login, otherwise, display error.
     app.post("/api/signup", function (req, res) {
-
         // Inspecting req.body
         // console.log("\n >> app.post('/api/signup'...) >> req.body: \n\n", req.body);
-
         db.User.create(req.body)
             .then(function (user) {
                 res.status(200).json('/login');
@@ -35,14 +27,10 @@ module.exports = function (app) {
                 console.log("\n >> app.post('/api/signup'...) >> error:\n\n", error);
                 res.json(error);
             });
-
     });
-
     app.get("/api/user", function (req, res) {
-
         // Inspecting req.user
         // console.log("\n >> app.get('api/users/:id'...) >> req.user:\n\n", req.user);
-
         // If user not logged in, send empty object
         if (!req.user) {
             res.redirect('/login')
@@ -68,15 +56,11 @@ module.exports = function (app) {
                 });
             });
         };
-
     });
-
     // API 'GET' *Logged-On User* Route 
     app.get("/api/users/:id", function (req, res) {
-
         // Inspecting req.user
         // console.log("\n >> app.get('api/users/:id'...) >> req.user:\n\n", req.user);
-
         // If user not logged in, send empty object
         if (!req.user) {
             res.json({});
@@ -92,39 +76,31 @@ module.exports = function (app) {
                 res.json(dbUser);
             });
         };
-
     });
-
     // API 'GET' Route To *Fetch All Posts*
     app.get("/api/posts", function (req, res) {
-
         db.Post.findAll({ order: Sequelize.col('createdAt', 'DESC') })
             .then(function (data) {
                 var hbsObject = { posts: data };
                 res.render("profile", hbsObject);
             });
-
     });
-
     // API 'POST' Route To Create *New Post*
     app.post("/api/posts", function (req, res) {
-
         // Inspecting req.body
         console.log("\n >> app.get('/api/posts'...) >> req.body:\n\n", req.body);
         console.log("\n >> app.get('/api/posts'...) >> req.user:\n\n", req.user);
         db.Post.create({
             body: req.body.body,
-            category: req.body.category
+            category: req.body.category,
+            UserId: req.user.id
         })
             .then(function (results) {
                 res.end();
             });
-
     });
-
     // API 'PUT' Route for *Post Update*
     app.put("/api/posts/:id", function (req, res) {
-
         db.Post.update(req.body,
             { where: { id: req.params.id } }
         ).then(function (result) {
@@ -133,12 +109,9 @@ module.exports = function (app) {
             // console.log("\n >> app.put('/api/posts/:id...) >> Post updated successfully!\n");
             res.status(200).end();
         });
-
     });
-
     // API 'DELETE' Route To *Remove Post* Route
     app.delete("/api/posts/:id", function (req, res) {
-
         db.Post.destroy(
             { where: { id: req.params.id } }
         ).then(function (result) {
@@ -147,7 +120,5 @@ module.exports = function (app) {
             // console.log("\n >> app.delete('/api/posts/:id...) >> Post removed successfully!\n");
             res.status(200).end();
         });
-
     });
-
 };
