4b892ced72664fe10ad7ab655db4ee860ef8a5b7
Annie Schaefer <annie@Annies-MacBook-Pro.local>
2018-04-30 17:30:27 -0400

    working on user page

--- public/js/user.js
+++ public/js/user.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
     
-    var userProfile = fetchUser();  
+    var userInfo = fetchUser();  
 
+     // Function To Retrieve Logged In User Info
+    function fetchUser() {
+
+      $.ajax("/api/user", {
+        method: 'GET'
+      })
+        .then(function (data) {
+          console.log('\n\n\n',data,'\nfetcheddata');
+  
+          if (data === "/login") {
+            var url = window.location.origin + data;
+            window.location.assign(url);
+            return;
+          }
+  
+          // return data;
+          res.render('user', {
+            name: data.name,
+            email: data.email,
+            occupation: data.occupation,
+            relationship: data.relationshipType,
+            location: data.location
+          })
+        });
+    };
 
     $.ajax("/api/user", {
         method: 'GET'
       })
         .then(function (data) {
-  
+  console.log("apiuser return", data.email);
           if (data === "/login") {
             var url = window.location.origin + data;
             window.location.assign(url);
@@ -17,25 +42,8 @@ $(document).ready(function(){
           return data;
   
         });
-        // Function To Retrieve Logged In User Info
-  function fetchUser() {
-
-    $.ajax("/api/user", {
-      method: 'GET'
-    })
-      .then(function (data) {
-
-        if (data === "/login") {
-          var url = window.location.origin + data;
-          window.location.assign(url);
-          return;
-        }
-
-        return data;
-
-      });
-      
-
-  };
-  console.log(userProfile)
+       
+  
+  
 })
+
